0cf9fb55ed11d3898c4f70fc3e8a4e20ad90ea04
Greg Willson <greg@ecocity.com>
2026-05-20 08:54:14 -0600

    new

--- node_modules/@quasar/app-vite/types/configuration/mobile-conf.d.ts
+++
@@ -1,7 +0,0 @@
-export interface QuasarMobileConfiguration {
-  /** Manually specify Android Studio bin path, in case Quasar isn't able to locate it on your system */
-  bin?: {
-    linuxAndroidStudio?: string;
-    windowsAndroidStudio?: string;
-  }
-}
--- node_modules/algosdk/dist/esm/types/transactions/heartbeat.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=heartbeat.js.map
